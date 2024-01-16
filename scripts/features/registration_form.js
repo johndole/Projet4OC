3061776cf19b5520dcd44252bb3297327d5cb226
johndole <isildur64500@gmail.com>
2024-01-16 17:56:37 +0100

    Removed unecessary spaces and comments

--- scripts/features/registration_form.js
+++ scripts/features/registration_form.js
@@ -1,32 +1,20 @@
-
 const form = document.querySelector("form[name='reserve']");
 
-
 export function clearRegistrationForm() {
-    form.reset();
-    console.log("Form cleared");  
+  form.reset();
+  console.log("Form cleared");
 }
 
- export function registrationFormData() {
-   // Create a FormData object from the form
-   const formData = new FormData(form);
+export function registrationFormData() {
+  // Create a FormData object from the form
+  const formData = new FormData(form);
 
-   // Create a user object to group the form data
-   const user = {};
-   formData.forEach((value, key) => {
-       user[key] = value;
-   });
-
-   // Log the user object to the console
-   console.log('Form submitted with data:', user);
-
-}
-
-
-  // Envoie les données de l'utilisateur au serveur
-  
-  /*const response = await fetch("http://example.org/post", {
-    method: "POST",
-    body: formData,
+  // Create a user object to group the form data
+  const user = {};
+  formData.forEach((value, key) => {
+    user[key] = value;
   });
-  console.log(await response.json());*/
+
+  // Log the user object to the console
+  console.log("Form submitted with data:", user);
+}
